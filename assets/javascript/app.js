1ab96b47d201f08649a14ae6bb598c1fa86221a4
Joshua Duncan <jduncan97@gmail.com>
2020-04-02 16:40:52 -0600

    Added JS to enable the buttons to pull from the API and display one result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 //Create a variable array to hold default buttons initially then added buttons
-var animals = ["Falcon", "Chicken", "Tiger", "Snake", "Sugar Glider"];
+var animals = ["Falcon", "Chicken", "Tiger", "Snake", "Sugar_Glider"];
+
 
 function renderButtons(){
     //deletes the buttons before loading old and new buttons or it would duplicate.
@@ -20,25 +21,31 @@ function renderButtons(){
 }
 renderButtons();
 
+//We need to click on an image and have it pull the gifs from giphy.
+
 function displayAnimalGif() {
     var animal = $(this).attr("data-name");
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=Kq0p36UhODAnuZDZa2VYKZ2XwlC6XzHw&limit=10";
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
+    $.ajax({ url: queryUrl, method: "GET" }).then(function(response) {
         console.log(response);
 
-    var gifDiv = $("<div class='animal'>")
+        var gifDiv = $("<div class='gifStore'>")
 
-    var rating = response.Rated;
-
-    var displayRating = $("<p>").text("Rating: " + rating);
-
-    gifDiv.append(displayRating);
-
+        var rating = response.data[0].rating;
+        var displayRating = $("<p>").text("Rating: " + rating);
+        gifDiv.append(displayRating);
+    
+        var gifs = response.data[0].images.downsized.url;
+        var displaygifs = $("<img>").attr("src", gifs);
+        gifDiv.append(displaygifs);
         
+        $("#gifsView").prepend(gifDiv)
+        
+        console.log(gifs)
 
     });
 };
+
+$(document).on("click", ".animal", displayAnimalGif);
+$(document).on("click", ".gifStore", displayAnimalGif);
