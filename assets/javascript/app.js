5d7744a8728df504ca1bc98aa9d158b91fc64305
Joshua Duncan <jduncan97@gmail.com>
2020-04-03 19:58:52 -0600

    Added a background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,20 @@
 //Create a variable array to hold default buttons initially then added buttons
-var animals = ["Falcon", "Chicken", "Tiger", "Snake", "Sugar_Glider"];
+var games = ["The Witcher 3", "Cyberpunk 2077", "Titanfall 2", "Breath of the Wild", "God of War", "The Last of Us", "Doom 2016",];
 
 
 function renderButtons(){
     //deletes the buttons before loading old and new buttons or it would duplicate.
     $("#buttons-view").empty()
-    //loops through the array of animals
-    for (var i = 0; i < animals.length; i++) {
+    //loops through the array of games
+    for (var i = 0; i < games.length; i++) {
         //this code is all jquery needs to generate a button
         var a = $("<button>");
-        //adds a class of animal to our button
-        a.addClass("animal");
+        //adds a class of game to our button
+        a.addClass("game");
         //adds a data-attribute
-        a.attr("data-name", animals[i]);
+        a.attr("data-name", games[i]);
         //provided the initial button text
-        a.text(animals[i]);
+        a.text(games[i]);
         //adds the button to the buttons-view dive
         $("#buttons-view").append(a);
         
@@ -24,10 +24,10 @@ renderButtons();
 
 //We need to click on an image and have it pull the gifs from giphy.
 
-function displayAnimalGif() {
+function displaygameGif() {
     $("#gifsView").empty();
-    var animal = $(this).attr("data-name");
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=Kq0p36UhODAnuZDZa2VYKZ2XwlC6XzHw&limit=10";
+    var game = $(this).attr("data-name");
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=Kq0p36UhODAnuZDZa2VYKZ2XwlC6XzHw&limit=10";
    
     $.ajax({ url: queryUrl, method: "GET" }).then(function(response) {
         console.log(response);
@@ -35,7 +35,7 @@ function displayAnimalGif() {
         for (i = 0; i < response.data.length; i++) {
             var gifDiv = $("<div class='gifStore'>");
 
-            var gifs = response.data[i].images.fixed_height_still.url;
+            var gifs = response.data[i].images.fixed_height_downsampled.url;
             //gifs.addClass("gif");
             var displaygifs = $("<img>").attr("src", gifs);
             gifDiv.append(displaygifs);
@@ -52,14 +52,14 @@ function displayAnimalGif() {
 
 
 
-$("#add-animal").on("click", function(event) {
+$("#add-game").on("click", function(event) {
     event.preventDefault();
-    var addChar = $("#animal-input").val().trim();
-    animals.push(addChar);
+    var addChar = $("#game-input").val().trim();
+    games.push(addChar);
     renderButtons();
 });
 
-$(document).on("click", ".animal", displayAnimalGif);
+$(document).on("click", ".game", displaygameGif);
 $(document).on("click", ".gif", function(){
     $(this).attr('src', $(this).data.images.fixed_height_downsampled);
 });
